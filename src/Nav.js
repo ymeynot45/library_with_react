750f4920098cb9b03e5786698f9a3b2502ed7fad
William Karsten <yme_ynot45@hotmail.com>
2022-01-21 14:18:40 -0600

    initial nav setup

--- src/Nav.js
+++ src/Nav.js
@@ -1,2 +1,16 @@
 import React from "react";
 
+const Nav = () => (
+    <nav className="App-nav">
+        <ul>
+            <li className="App-nav-button">
+                <button>Books</button>
+            </li>
+            <li className="App-nav-button">
+                <button>Add book</button>
+            </li>
+        </ul>
+    </nav>
+);
+
+export default Nav;
