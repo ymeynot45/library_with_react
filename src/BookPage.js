78de494b296d8721a9321f4ef97930ecac8378dc
William Karsten <yme_ynot45@hotmail.com>
2022-01-26 11:22:58 -0600

    I learned that I MUST have let before establishing a var.  I set up the constuctor and commented out the preset books.

--- src/BookPage.js
+++ src/BookPage.js
@@ -20,54 +20,64 @@ let Book = function (title, author, pageNumber, haveRead) {
     return this
 }
 
+const addBookToLibrary = function(library, newBook) {
+    addIdToBook(newBook);
+    library = library.push(newBook);
+    findNextId();
+    return library;
+}
+const addIdToBook = function(newbook) {
+    let nextBookId = findNextId();
+    console.log(nextBookId);
+    newbook['id'] = nextBookId;
+    return newbook
+}
 
-
-
-// const loadLibrary = function(library) {
-//     let thirdShelf = document.createElement("ol");
-//     thirdShelf.setAttribute(`id`, `thirdShelf`);
-//     document.getElementById(`secondShelf`).appendChild(thirdShelf);
-//     library.forEach(book => {
-//         createBookSlot(book);
-//     });
-// }   
-
+const findNextId = function() {
+    let nextId = Math.max(...library.map(book => book.id));
+    if(nextId < 1){
+        nextId = 1
+    }
+    else{
+        ++nextId
+    }
+    return nextId;
+}
 
 
 
 let library = [
-    {
-        id: 1,
-        title: "The Hobbit",
-        author: "J.R.R. Tolkien",
-        pageCount: 295,
-        haveRead: false
+    // {
+    //     id: 1,
+    //     title: "The Hobbit",
+    //     author: "J.R.R. Tolkien",
+    //     pageCount: 295,
+    //     haveRead: false
 
-    },
-    {
-        id: 2,
-        title: "Flight from the Dark",
-        author: "Joe Denver",
-        pageCount: 300,
-        haveRead: true
+    // },
+    // {
+    //     id: 2,
+    //     title: "Flight from the Dark",
+    //     author: "Joe Denver",
+    //     pageCount: 300,
+    //     haveRead: true
 
-    },
-    {
-        id: 3,
-        title: "Fire on the Water",
-        author: "Gary Chalk",
-        pageCount: 350,
-        haveRead: false
+    // },
+    // {
+    //     id: 3,
+    //     title: "Fire on the Water",
+    //     author: "Gary Chalk",
+    //     pageCount: 350,
+    //     haveRead: false
 
-    }
+    // }
 ];
 
 
 
-
+addBookToLibrary(library, new Book("The Hobbit", "J.R.R. Tolkien", 295, false));
+addBookToLibrary(library, new Book("Flight from the Dark", "Joe Denver", 300, true));
+addBookToLibrary(library, new Book("Fire on the Water", "Gary Chalk", 350, false));
 
 
 export {library};
-// addBookToLibrary(myLibrary, new Book("The Hobbit", "J.R.R. Tolkien", 295, false));
-//     addBookToLibrary(myLibrary, new Book("Flight from the Dark", "Joe Denver", 300, true));
-//     addBookToLibrary(myLibrary, new Book("Fire on the Water", "Gary Chalk", 350, false));
